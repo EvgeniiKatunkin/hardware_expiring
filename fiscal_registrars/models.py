7f8a3a13d0aa6ad8f12798161aa1813203f17439
EvgeniiKatunkin <evgeniikatunkin@gmail.com>
2021-07-02 09:03:28 +0500

    Comment is added

--- fiscal_registrars/models.py
+++ fiscal_registrars/models.py
@@ -10,3 +10,6 @@ class Address(models.Model):
     """Модель адреса. Внешний ключ города. Поле с текстом названия улицы и номером дома."""
     models.ForeignKey(City, on_delete=models.CASCADE)
     address = models.CharField(max_length=100)
+
+
+"""Можно продолжить если прям сильно захочется. ;)"""
